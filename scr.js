e0ca3a0e1dc063cf78ae00d7456fad771a5368b1
sharmi4590 <bheemisettysharmila@gmail.com>
2023-05-26 01:44:54 +0530

    Files upload

---
+++ scr.js
@@ -0,0 +1,12 @@
+document.querySelector('.first-button').addEventListener('click', function () {
+
+    document.querySelector('.animated-icon1').classList.toggle('open');
+    });
+    document.querySelector('.second-button').addEventListener('click', function () {
+    
+    document.querySelector('.animated-icon2').classList.toggle('open');
+    });
+    document.querySelector('.third-button').addEventListener('click', function () {
+    
+    document.querySelector('.animated-icon3').classList.toggle('open');
+    });
